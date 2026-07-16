b829db935e99951a43cbab5622f48eda7c5351c9
luisilloelpillo55 <2023371058@uteq.edu.mx>
2026-07-15 19:39:09 -0600

    Avances frontend

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,6 +4,9 @@ import { Layout } from './pages/layout/layout';
 import { Home } from './pages/home/home';
 import { MiAgenda } from './pages/mi-agenda/mi-agenda';
 import { Catalogo } from './pages/catalogo/catalogo';
+import { Admin } from './pages/admin/admin';
+import { AvisosBoard } from './pages/avisos-board/avisos-board';
+import { SesionForo } from './pages/sesion-foro/sesion-foro';
 import { authGuard } from './core/guards/auth-guard';
 import { publicGuard } from './core/guards/auth-guard';
 import { Register } from './pages/register/register';
@@ -28,10 +31,13 @@ export const routes: Routes = [
     canActivateChild: [authGuard],
     children: [
       { path: 'home', component: Home },
-      // EP-03/04: Mi Agenda (Tutor: HU-09/10/11/12 + Alumno: HU-15/16)
-      { path: 'mi-agenda', component: MiAgenda },
+      // EP-03/04: Mis Tutorías (Tutor: HU-09/10/11/12 + Alumno: HU-15/16)
+      { path: 'mis-tutorias', component: MiAgenda },
       // EP-04: Catálogo de tutorías (HU-13/14)
       { path: 'catalogo', component: Catalogo },
+      { path: 'admin', component: Admin },
+      { path: 'avisos', component: AvisosBoard },
+      { path: 'sesion/:id/foro', component: SesionForo },
       // Aquí agregarán las rutas de los demás compañeros (admin, etc.)
     ],
   },
